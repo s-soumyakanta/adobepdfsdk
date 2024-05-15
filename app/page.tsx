f432f328c113f0d1067e94fbf1bacd75ffa4b7d8
S Soumyakanta <ssoumyakanta@fightright.in>
2024-05-15 19:24:58 +0530

    final domain check

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import AdobePdfViwer from "./components/AdobePdfViwer"
 
 const page = () => {
   return (
+    <>
     <div>Hello World!</div>
+    <div>
+    <AdobePdfViwer />
+    </div>
+    </>
   )
 }
 
