d530d59ffa96541122bc8ec425f9b2e34d3cff18
S Soumyakanta <ssoumyakanta@fightright.in>
2024-05-24 16:09:52 +0530

    adlfjaf

--- app/pdfreader/page.tsx
+++ app/pdfreader/page.tsx
@@ -1,10 +1,17 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import AdobeFilePreviewDialog from "../components/AdobeFilePreviewDialog";
 import AdobePdfViewer from "../components/AdobePdfViewer";
 import { useBoolean } from "../hooks/use-boolean";
 
 const page = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(()=>{
+    setDocUrl('https://dbegkrglt48mx.cloudfront.net/cms_documents/qwertyuiop.pdf')
+  },[])
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [docUrl, setDocUrl] = useState<string>("")
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const adobeFilePreview = useBoolean();
   const fileUrl = 'https://dbegkrglt48mx.cloudfront.net/cms_documents/qwertyuiop.pdf'
@@ -15,18 +22,18 @@ const page = () => {
           <div>
             <h1>Adobe PDF Viwer</h1>
           </div>
-          {/* <div>
+          <div>
             <button onClick={() => adobeFilePreview.onTrue()}>Open</button>
             <button onClick={() => adobeFilePreview.onFalse()}>Close</button>
-          </div> */}
+          </div>
         </div>
-        {/* <AdobeFilePreviewDialog
+        <AdobeFilePreviewDialog
           onClose={adobeFilePreview.onFalse}
-          open={true}
+          open={adobeFilePreview.value}
           fileUrl={fileUrl}
           fileName="Test file"
-        /> */}
-         <AdobePdfViewer pdfUrl={fileUrl} fileName="Test File" /> 
+        />
+         <AdobePdfViewer pdfUrl={docUrl} fileName="Test File" /> 
       </div>
     </>
   );
